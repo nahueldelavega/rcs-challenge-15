95297c9f9827afcd54dd32c7889e665f53cc9c87
Nahuel de la Vega <nahueldlvega@gmail.com>
2022-04-25 16:49:07 -0300

    listo challenge 15

---
+++ js/app.js
@@ -0,0 +1,7 @@
+alert('Queres jugar a los dados?');
+let random1 = Math.round((Math.random())*6);
+console.log(random1);
+
+let random2 = Math.round((Math.random())*6);
+console.log(random2);
+console.log('Tu numero es el ' + (random1+random2));
